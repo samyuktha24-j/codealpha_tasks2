59caea5e4ae71aa361deaa882a8782a5057bf54b
samyuktha24-j <samyukthamaadhavan@gmail.com>
2025-05-18 20:45:30 +0530

    Add files via upload

---
+++ PortfolioSecurityProject/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Welcome to my Portfolio!');
+});
+
+function changeColor() {
+    document.getElementById('projects').style.backgroundColor = 'lightblue';
+}
